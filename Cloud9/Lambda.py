b8b4f0ac3b16648cc988aaa66930f68d0bf3c338
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-01-15 16:37:22 -0800

    Update Lambda.py

--- Cloud9/Lambda.py
+++ Cloud9/Lambda.py
@@ -10,4 +10,21 @@ def lambda_handler(event, context):
     es = boto3.client('es')
     
     # Run the search query
-    response = es.search
+    response = es.search(
+        index='my-search-index',
+        body={
+            'query': {
+                'match': {
+                    '_all': query
+                }
+            }
+        }
+    )
+    
+    # Format the search results
+    results = []
+    for hit in response['hits']['hits']:
+        results.append(hit['_source'])
+    
+    # Return the search results to the user
+    return {'results': results}
